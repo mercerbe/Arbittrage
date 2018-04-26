cbea49a4892bcc988f6e26b72cc7678996bff9c2
Admin <mercerbe@gmail.com>
2018-04-26 17:14:02 -0400

    chart placement/css modified

--- assets/javascript/coinpagelogic.js
+++ assets/javascript/coinpagelogic.js
@@ -92,8 +92,8 @@ var chart = new Chart(ctx, {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [{
             label: "My First dataset",
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgb(48,180,112)',
+            borderColor: 'rgba(12, 12, 37, 1.0)',
             data: [0, 10, 5, 2, 20, 30, 45],
         }]
     },
