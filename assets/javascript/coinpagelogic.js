f02e1424cb5bf76f14d610ba9c615cd5791b4ad8
Admin <mercerbe@gmail.com>
2018-04-28 12:08:42 -0400

    all logos in array and looped on landing page

--- assets/javascript/coinpagelogic.js
+++ assets/javascript/coinpagelogic.js
@@ -2,10 +2,15 @@ $(document).ready(function() {
   //backstretch
   $(".jumbotron").backstretch("https://media.giphy.com/media/3oEjI9ZALbbWcO1hpm/giphy.gif");
   //Arrays
-  var cryptoArrayById = ["bitcoin", "etheruem", "ripple", "bitcoin-cash", "eos", "litecoin", "cardano", "stellar", "iota", "neo", "tron", "monero", "dash", "nem", "tether", "ethereum-classic", "vechain", "omisego", "qtum",
-   "binance-coin", "icon", "bitcoin-gold", "lisk", "zcash", "verge", "steem", "bytecoin-bcn", "bytom", "nano", "bitcoin-private"];
+  var cryptoArrayById = [{"id": "bitcoin", "logo":"assets/images/bitcoin.png"}, {"id":"etheruem", "logo":"assets/images/eth.png"}, {"id":"ripple", "logo":"assets/images/xrp.png"}, {"id":"bitcoin-cash", "logo":"assets/images/bitcoin-cash.png"},
+   {"id":"eos", "logo":"assets/images/eos.png"}, {"id":"litecoin", "logo":"assets/images/litecoin.png"}, {"id":"cardano", "logo":"assets/images/cardano.png"}, {"id":"stellar", "logo":"assets/images/stellar.png"}, {"id":"iota", "logo":"assets/images/iota.png"},
+   { "id":"neo", "logo":"assets/images/neo.png"}, {"id":"tron", "logo":"assets/images/tron.png"}, {"id":"monero", "logo":"assets/images/monero.png"}, {"id":"dash", "logo":"assets/images/dash.png"},{"id":"nem", "logo":"assets/images/nem.png"},
+   {"id":"tether", "logo":"assets/images/tether.png"}, {"id":"ethereum-classic", "logo":"assets/images/ethereum-classic.png"}, {"id":"vechain", "logo":"assets/images/vechain.svg"}, {"id":"omisego", "logo":"assets/images/omisego.svg"},
+   {"id":"qtum", "logo":"assets/images/qtum.png"}, {"id":"binance-coin", "logo": "assets/images/binance-coin.svg"}, {"id":"icon", "logo":"assets/images/icon.png"}, {"id":"bitcoin-gold", "logo":"assets/images/bitcoin-gold.png"},
+   {"id":"lisk", "logo":"assets/images/lisk.png"}, {"id":"zcash", "logo":"assets/images/zcash.png"}, {"id":"verge", "logo":"assets/images/verge.png"}, {"id":"steem", "logo":"assets/images/steem.png"}, {"id":"bytecoin-bcn", "logo":"assets/images/bytecoin-bcn.png"},
+   {"id":"bytom", "logo":"assets/images/bytom.png"}, {"id":"nano", "logo":"assets/images/nano.jpg"}, {"id":"bitcoin-private", "logo":"assets/images/bitcoin-private.png"}];
   var exchangeArray = ["BitFinex", "Bitstamp", "Bittrex", "C-Cex", "Cex.io", "Exmo", "Hitbtc", "Kraken", "Livecoin", "Poloniex", "wexnz", "YoBit"];
-  var exchangesBenWouldLikeToAdd = ["Binance", "Kucoin", "Coinbase"]
+  var exchangesBenWouldLikeToAdd = ["Binance", "Kucoin", "Coinbase"];
   //for loop for filter sidebar
   for (var i = 0; i < exchangeArray.length; i++) {
     var exchangeFilter = $("<a class='btn btn-success exchangeFilter'>");
@@ -16,6 +21,7 @@ $(document).ready(function() {
   //for loop for landing page coin images/Links
   for (var i = 0; i < cryptoArrayById.length; i++) {
     var coinThumbnail = $("<img src='http://fakeimg.pl/200x200/' class='img-fluid img-thumbnail coinImg btn'>");
+    coinThumbnail.attr('src', cryptoArrayById[i].logo);
     $("#easyPaginate").append(coinThumbnail);
     coinThumbnail.on("click", function() {
       console.log("click");
