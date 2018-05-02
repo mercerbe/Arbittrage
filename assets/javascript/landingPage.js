073fa8a0cca1f62a87a6fb664de2890035e6902e
Admin <mercerbe@gmail.com>
2018-05-02 15:07:49 -0400

    css adjustments to master for landing page

--- assets/javascript/landingPage.js
+++ assets/javascript/landingPage.js
@@ -50,59 +50,59 @@ let easyPaginate = document.getElementById("easyPaginate");
 //login modal
 
 //signIn
-// var txtEmail = document.getElementById('txtEmail');
-// var txtPass = document.getElementById('txtPass');
-// var btnLogin = document.getElementById('btnLogin');
-// var btnSignup = document.getElementById('btnSignup');
-// var btnLogout = document.getElementById('btnLogout');
-// var btnShowModal = document.getElementById('btnShowModal');
+var txtEmail = document.getElementById('txtEmail');
+var txtPass = document.getElementById('txtPass');
+var btnLogin = document.getElementById('btnLogin');
+var btnSignup = document.getElementById('btnSignup');
+var btnLogout = document.getElementById('btnLogout');
+var btnShowModal = document.getElementById('btnShowModal');
 
 //login
-// btnLogin.addEventListener('click', e => {
-// //get email and pass
-// var email = txtEmail.value;
-// var pass = txtPass.value;
-// var auth = firebase.auth();
-// //sign in
-// var promise = auth.signInWithEmailAndPassword(email, pass);
-// promise.catch(e => console.log(e.message));
-// promise.catch(e => alert("Not signed up. Please sign up first!"));
-//
-// })
+btnLogin.addEventListener('click', e => {
+//get email and pass
+var email = txtEmail.value;
+var pass = txtPass.value;
+var auth = firebase.auth();
+//sign in
+var promise = auth.signInWithEmailAndPassword(email, pass);
+promise.catch(e => console.log(e.message));
+promise.catch(e => alert("Not signed up. Please sign up first!"));
+
+})
 
 //sign up
-// btnSignup.addEventListener('click', e => {
-// var email = txtEmail.value;
-// var pass = txtPass.value;
-// var auth = firebase.auth();
-// console.log("click");
-// //sign in
-// var promise = auth.createUserWithEmailAndPassword(email, pass);
-// promise.catch(e => console.log(e.message));
-// })
+btnSignup.addEventListener('click', e => {
+var email = txtEmail.value;
+var pass = txtPass.value;
+var auth = firebase.auth();
+console.log("click");
+//sign in
+var promise = auth.createUserWithEmailAndPassword(email, pass);
+promise.catch(e => console.log(e.message));
+})
 
 //real time auth listener
-// firebase.auth().onAuthStateChanged(firebaseUser => {
-// if (firebaseUser) {
-//    console.log(firebaseUser);
-//    $("#modal").modal('hide');
-// } else {
-//    console.log("not logged in");
-// }
-// })
+firebase.auth().onAuthStateChanged(firebaseUser => {
+if (firebaseUser) {
+   console.log(firebaseUser);
+   $("#modal").modal('hide');
+} else {
+   console.log("not logged in");
+}
+})
 //logout
-// btnLogout.addEventListener('click', e => {
-// firebase.auth().signOut();
-// setTimeout(function() {
-//    $("#modal").modal('show');
-// }, 1000);
-//
-// })
-// //show modal
-// btnShowModal.addEventListener('click', e => {
-// console.log("click");
-// setTimeout(function() {
-//    $("#modal").modal('show');
-// }, 100);
-// })
+btnLogout.addEventListener('click', e => {
+firebase.auth().signOut();
+setTimeout(function() {
+   $("#modal").modal('show');
+}, 1000);
+
+})
+//show modal
+btnShowModal.addEventListener('click', e => {
+console.log("click");
+setTimeout(function() {
+   $("#modal").modal('show');
+}, 100);
+})
  });
